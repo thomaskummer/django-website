36fb6b1ac1ddbb94a01376b8311c7ecce0120a5f
thomaskummer <kummetho@gmail.com>
2022-04-25 11:56:01 +0200

    Commit with green menu bars, just before switching to a lighter color scheme.

--- polls/views.py
+++ polls/views.py
@@ -15,6 +15,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
+
 """
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
